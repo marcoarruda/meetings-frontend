c7b776fae10d5fc292d268371ba816411df622ab
Rodrigo Sousa <rodrigudesousa@gmail.com>
2020-01-30 14:33:42 -0400

    calendar inicio

--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import vueresource from 'vue-resource';
+import DaySpanVuetify from 'dayspan-vuetify'
 
 Vue.config.productionTip = false
 
 Vue.use(vueresource);
+
+Vue.use(vuetify);
+
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => '#1976d2'
+  }
+});
 
 new Vue({
   router,
